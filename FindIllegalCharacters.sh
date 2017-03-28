0bee1dff143d75918a1132254e49d4b825248dda
mikemuir <mike@muirsandwich.com>
2017-03-28 13:10:13 -0700

    Updated to pass argument
    Usage: FindIllegalCharacters.sh /Path/To/Scrape/

--- FindIllegalCharacters.sh
+++ FindIllegalCharacters.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Find Illegal Characters
+#This script will parse all items in a directory passed during argument and output its location.
+#Usage: FindIllegalCharacters.sh /Path/To/Scrape/
+
 # Make sure that the script understands space characters.
 IFS=$'\n'
 
 # Use grep to locate all files with a space at the end of its name with the Regular Expression
 # .*[<>'\':*?|].*
-find . -mindepth 1 | while read ffile || test -n "$ffile"; do
+find $1 -mindepth 1 | while read ffile || test -n "$ffile"; do
 	echo $ffile | grep ".*[<>'\':*?|].*"
 done
 
